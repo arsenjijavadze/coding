af76833eeabaafca348287a5f26e4ce4a85b85a5
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-18 00:17:53 +0300

    feat: add binary addition

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,14 +1,25 @@
-// Count the divisors of a number
+//* Count the divisors of a number
 
-function getDivisorsCnt(n) {
+// function getDivisorsCnt(n) {
 
-    let divisors = 0;
-    mod = n;
-    while (mod > 0) {
-        if (n % mod === 0) {
-            divisors++;
-        }
-        mod--;
-    }
-    return divisors;
-}
+//     let divisors = 0;
+//     mod = n;
+//     while (mod > 0) {
+//         if (n % mod === 0) {
+//             divisors++;
+//         }
+//         mod--;
+//     }
+//     return divisors;
+// }
+//*
+
+
+//* Kata Binary Addition
+
+// function addBinary(a, b) {
+//     return (a + b).toString(2);
+
+// }
+let addBinary = (a, b) => (a + b).toString(2);
+console.log(addBinary(3, 6));git
