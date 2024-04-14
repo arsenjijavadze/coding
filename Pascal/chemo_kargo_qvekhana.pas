3411a1c5d304186010ac466de56c50abd56c4822
arsenjijavadze <arsen.jijavadze@gmail.com>
2024-04-14 16:32:52 +0300

    feat: add new examples

---
+++ Pascal/chemo_kargo_qvekhana.pas
@@ -0,0 +1,10 @@
+program chemo_kargo_qvekhana;
+begin
+    writeln('ჩემო კარგო ქვეყანა');
+    readln;
+    writeln('რაზედ მოგიწყენია');
+    readln;
+    writeln('აწმყო თუ არა გვწყალობს');
+    readln;
+    writeln('მომავალი ჩვენია');
+end.
